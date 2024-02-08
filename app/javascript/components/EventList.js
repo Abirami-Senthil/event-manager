a091347bd799400fa52fdfa8e880526182d5a9d7
Abirami S <abirami.s@zetwerk.com>
2024-02-08 19:04:12 +0530

    Read functionality of CRUD app

--- app/javascript/components/EventList.js
+++ app/javascript/components/EventList.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const EventList = ({ events }) => {
-  const renderEvents = (eventArray) => {
-    eventArray.sort((a, b) => new Date(b.event_date) - new Date(a.event_date));
+    const renderEvents = (eventArray) => {
+      eventArray.sort((a, b) => new Date(b.event_date) - new Date(a.event_date));
+  
+      return eventArray.map((event) => (
+        <li key={event.id}>
+          <NavLink to={`/events/${event.id}`}>
+            {event.event_date}
+            {' - '}
+            {event.event_type}
+          </NavLink>
+        </li>
+      ));
+    };
 
-    return eventArray.map((event) => (
-      <li key={event.id}>
-        {event.event_date}
-        {' - '}
-        {event.event_type}
-      </li>
-    ));
-  };
-
-  return (
-    <section>
-      <h2>Events</h2>
-      <ul>{renderEvents(events)}</ul>
-    </section>
-  );
+    return (
+        <section className="eventList">
+          <h2>Events</h2>
+          <ul>{renderEvents(events)}</ul>
+        </section>
+      );
 };
 
 EventList.propTypes = {
